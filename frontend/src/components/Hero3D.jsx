b49babaaa19237b6bd2b56761cf0d6ca395d64a7
Nandha-0301 <nandhanpream20@gmail.com>
2026-07-30 22:23:58 +0530

    Self-host Spline 3D scene file

--- frontend/src/components/Hero3D.jsx
+++ frontend/src/components/Hero3D.jsx
@@ -5,7 +5,7 @@ const Hero3D = () => {
   return (
     <div className="w-full h-[420px] md:h-[500px] overflow-hidden rounded-2xl flex items-center justify-center relative">
       <div className="w-full h-full scale-[1.35] md:scale-[1.5] origin-center flex items-center justify-center">
-        <Spline scene="https://prod.spline.design/wA9xLzcW4J6b2zf2/scene.splinecode" className="w-full h-full" />
+        <Spline scene="/scene.splinecode" className="w-full h-full" />
       </div>
       <style>{`
         /* Hide Spline Logo if rendered as an absolute link */
